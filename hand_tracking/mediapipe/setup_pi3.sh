1b96bf24ce92af92c24ebaa08561e15776811454
Dawson X Bancroft <dawsonxbancroft@gmail.com>
2024-02-13 02:39:29 +0000

    Need to get mediapipe working

--- hand_tracking/mediapipe/setup_pi3.sh
+++ hand_tracking/mediapipe/setup_pi3.sh
@@ -1,5 +1,5 @@
-sudo apt install ffmpeg python3-opencv python3-pip
-sudo apt install libxcb-shm0 libcdio-paranoia-dev libsdl2-2.0-0 libxv1  libtheora0 libva-drm2 libva-x11-2 libvdpau1 libharfbuzz0b libbluray2 libatlas-base-dev libhdf5-103 libgtk-3-0 libdc1394-22 libopenexr25
+sudo apt install ffmpeg -y
+sudo apt install libxcb-shm0 libcdio-paranoia-dev libsdl2-2.0-0 libxv1  libtheora0 libva-drm2 libva-x11-2 libvdpau1 libharfbuzz0b libbluray2 libatlas-base-dev libhdf5-103 libgtk-3-0 libdc1394-22 libopenexr25 -y
 sudo pip install gtts
-sudo apt install mpg321
-sudo pip install mediapipe-rpi3==0.8.4.0
+sudo apt install mpg321 -y
+sudo pip install mediapipe-rpi3
---
+++ hand_tracking/mediapipe/setup_pi3_dxb.sh
@@ -0,0 +1,3 @@
+sudo pip install mediapipe-rpi3
+sudo pip install gtts
+sudo apt install mpg321 -y
